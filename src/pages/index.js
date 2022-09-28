5ebd016ece98f3f98f4e478f79fb983f60347bb7
dlshnkLK <dilshankaliyanage@yahoo.com>
2022-09-28 20:17:40 +0530

    f-c

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Nextjs and MDX blog</title>
+      </Head>
+      <main>
+        <h1 className='text-2xl text-red-400'>Hello</h1>
+      </main>
+    </div>
+  )
+}
